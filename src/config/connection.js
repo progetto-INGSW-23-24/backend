d0235cf587ed03c32b1fac0e83416553185b68e7
roberto ingenito <ingenitoroby@gmail.com>
2024-09-25 13:24:25 +0200

    aggiunta chiave db rds

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,20 +1,28 @@
 import { Sequelize } from "sequelize";
-import { DB_NAME, DB_USER, DB_PSW, DB_PORT, DB_SCHEMA, DB_HOST } from './environment.js'; 
+import * as fs from "fs";
+import { DB_NAME, DB_USER, DB_PSW, DB_PORT, DB_SCHEMA, DB_HOST } from './environment.js';
 
 const connection = new Sequelize({
-    database: DB_NAME, 
-    username: DB_USER, 
-    password: DB_PSW, 
-    host: DB_HOST, 
-    port: DB_PORT, 
-    schema: DB_SCHEMA, 
-    dialect: 'postgres'
+    database: DB_NAME,
+    username: DB_USER,
+    password: DB_PSW,
+    host: DB_HOST,
+    port: DB_PORT,
+    schema: DB_SCHEMA,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: true,
+            ca: fs.readFileSync('./config/dieti_deals_24_backend_key.pem').toString()
+        }
+    }
 })
 
 try {
-    await connection.authenticate() 
+    await connection.authenticate()
     console.log('connection ok');
-} catch(error) {
+} catch (error) {
     console.log(`Unable to connect: ${error}`);
 }
 
