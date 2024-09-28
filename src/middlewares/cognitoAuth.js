0e2a09d215c605c162919c5eac580f27422b0e1d
roberto ingenito <ingenitoroby@gmail.com>
2024-09-28 10:34:05 +0200

    corretto middleware autenticazione

--- src/middlewares/cognitoAuth.js
+++ src/middlewares/cognitoAuth.js
@@ -1,29 +1,59 @@
-import jwt from 'jsonwebtoken'; 
+import jwt from 'jsonwebtoken';
+import { COGNITO_REGION, COGNITO_USER_POOL_ID } from '../config/environment';
 
-const cognitoAuth = (req, res, next) => {
+let pems = {}; // Oggetto per memorizzare le chiavi
 
-    const token = req.headers['authorization']; 
+// Funzione per scaricare le chiavi pubbliche da AWS Cognito
+const getPems = async () => {
+    let region = COGNITO_REGION;
+    let userPoolId = COGNITO_USER_POOL_ID;
 
-    if(!token) {
-        return res.status(401).json({message: 'Missing Token'}); 
+    const url = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
+    const response = await axios.get(url);
+    const { keys } = response.data;
+
+    // Converti ciascun JWK in PEM e memorizzali
+    keys.forEach(key => {
+        const pem = jwkToPem(key);
+        pems[key.kid] = pem;
+    });
+};
+
+// Middleware di autenticazione
+const cognitoAuth = async (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token || !token.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Token mancante o non valido' });
     }
 
-    const decodedToken = jwt.decode(token); 
+    const jwtToken = token.split(' ')[1];
 
-    if(!decodedToken) {
-        return res.status(401).json({message: 'Token not Valid'}); 
+    try {
+        // Verifica il token JWT
+        const decodedToken = jwt.decode(jwtToken, { complete: true });
+        const kid = decodedToken.header.kid; // Recupera l'ID della chiave
+
+        if (!pems[kid]) {
+            // Se non abbiamo già memorizzato il PEM, scaricalo
+            await getPems();
+        }
+
+        const pem = pems[kid];
+
+        // Verifica la firma e l'integrità del token
+        jwt.verify(jwtToken, pem, { algorithms: ['RS256'] }, (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: 'Token non valido' });
+            }
+
+            // Se il token è valido, puoi inserire i dati dell'utente nella request
+            req.user = decoded;
+            next();
+        });
+    } catch (error) {
+        return res.status(401).json({ message: 'Token non valido' });
     }
-
-    const cognitoSub = decodedToken.sub;
-    console.log(`ID univoco dell\'utente: ${cognitoSub}`);
-
-    req.user = {
-        userId: cognitoSub,
-        email: decodedToken.email
-    }
-
-    next(); 
-
-}
+};
 
 export default cognitoAuth; 
