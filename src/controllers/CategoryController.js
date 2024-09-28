b5b5ab56e7106501ab4c46591f22329d6afbed36
roberto ingenito <ingenitoroby@gmail.com>
2024-09-28 13:36:20 +0200

    aggiunto router e controller categorie

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -7,7 +7,7 @@ class CategoryController {
         try {
             const categories = await Category.findAll();
 
-            return res.status(200).json(categories);
+            res.status(200).json(categories);
         } catch (error) {
             next(new HttpError(`Errore nel selezionare le categorie: ${error.message}`, 500));
         }
