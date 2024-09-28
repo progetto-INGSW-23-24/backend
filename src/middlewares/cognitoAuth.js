0dce6b47e98d5ef6de44fb1af54518a56be2cf4c
roberto ingenito <ingenitoroby@gmail.com>
2024-09-28 11:16:24 +0200

    corretto middleware autenticazione

--- src/middlewares/cognitoAuth.js
+++ src/middlewares/cognitoAuth.js
@@ -1,23 +1,5 @@
-import jwt from 'jsonwebtoken';
-import { COGNITO_REGION, COGNITO_USER_POOL_ID } from '../config/environment.js';
-
-let pems = {}; // Oggetto per memorizzare le chiavi
-
-// Funzione per scaricare le chiavi pubbliche da AWS Cognito
-const getPems = async () => {
-    let region = COGNITO_REGION;
-    let userPoolId = COGNITO_USER_POOL_ID;
-
-    const url = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/jwks.json`;
-    const response = await axios.get(url);
-    const { keys } = response.data;
-
-    // Converti ciascun JWK in PEM e memorizzali
-    keys.forEach(key => {
-        const pem = jwkToPem(key);
-        pems[key.kid] = pem;
-    });
-};
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+import { COGNITO_CLIENT_ID, COGNITO_USER_POOL_ID } from '../config/environment.js';
 
 // Middleware di autenticazione
 const cognitoAuth = async (req, res, next) => {
@@ -29,31 +11,19 @@ const cognitoAuth = async (req, res, next) => {
 
     const jwtToken = token.split(' ')[1];
 
+    // Verifier that expects valid access tokens:
+    const verifier = CognitoJwtVerifier.create({
+        userPoolId: COGNITO_USER_POOL_ID,
+        tokenUse: "access",
+        clientId: COGNITO_CLIENT_ID,
+    });
+
     try {
-        // Verifica il token JWT
-        const decodedToken = jwt.decode(jwtToken, { complete: true });
-        const kid = decodedToken.header.kid; // Recupera l'ID della chiave
+        const payload = await verifier.verify(jwtToken);
 
-        if (!pems[kid]) {
-            // Se non abbiamo già memorizzato il PEM, scaricalo
-            await getPems();
-        }
-
-        const pem = pems[kid];
-
-        // Verifica la firma e l'integrità del token
-        jwt.verify(jwtToken, pem, { algorithms: ['RS256'] }, (err, decoded) => {
-            if (err) {
-                return res.status(401).json({ message: 'Token non valido' });
-            }
-
-            // Se il token è valido, puoi inserire i dati dell'utente nella request
-            req.user = {
-                userId: decodedToken.sub,
-            };
-            next();
-        });
-    } catch (error) {
+        req.user = { userId: payload.sub };
+        next();
+    } catch {
         return res.status(401).json({ message: 'Token non valido' });
     }
 };
